a5c7c49c8de5a78c3c7ee4254f2d9febc8a69069
Geoffrey Gilmore <ggilmore@mit.edu>
2015-04-08 16:13:00 -0400

    half broken thing

--- DotaBlazeReal.py
+++ DotaBlazeReal.py
@@ -1,31 +1,17 @@
 from flask import Flask, render_template
 import requests
+from Helpers import processResponse
+
 
 app = Flask(__name__)
 
 
-@app.route('/dota2')
+@app.route('/')
 def index():
     r = requests.get("https://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v0001/?key=12B9C2C08AAC635D3A305D7D26793738")
-    return render_template("index.html", games=processResponse(r.json()[u"result"][u"games"]))
-
-
-def processResponse(arr):
-    processedResponse = map(getMatchInfo, arr)
-    return processedResponse
-
-def getTeamName(isRadiant, dotaSideObj):
-    if (isRadiant):
-        result = dotaSideObj.get(u"radiant_team",{})
-    else:
-        result = dotaSideObj.get(u"dire_team",{})
-    return result.get(u"team_name", u"No Team Name")
-
-
-def getMatchInfo(game):
-    # return (game[u"match_id"], game[u"dire_team"][u"team_name"], game[u"radiant_team"][u"team_name"])
-    return {u"match_id": game[u"match_id"], u"radiant": getTeamName(True, game), u"dire": getTeamName(False, game)}
-
+    games = processResponse(r.json()[u"result"][u"games"])
+    print games
+    return render_template("index.html", games)
 
 if __name__ == '__main__':
     app.run()
---
+++ Helpers.py
@@ -0,0 +1,24 @@
+__author__ = 'gmgilmore'
+from TowerStatusTracker import towerStatus
+
+def processResponse(arr):
+    processedResponse = map(getMatchInfo, arr)
+    print processedResponse
+    return processedResponse
+
+def getTeamName(isRadiant, dotaSideObj):
+    if (isRadiant):
+        result = dotaSideObj.get(u"radiant_team",{})
+    else:
+        result = dotaSideObj.get(u"dire_team",{})
+    return result.get(u"team_name", u"No Team Name")
+
+
+def getMatchInfo(game):
+    # return (game[u"match_id"], game[u"dire_team"][u"team_name"], game[u"radiant_team"][u"team_name"])
+    dictionary =  {u"match_id": game[u"match_id"],
+            u"radiant": getTeamName(True, game),
+            u"dire": getTeamName(False, game),
+            u"dire_tower_status": towerStatus(False, bin(game[u"scoreboard"][u"dire"][u"tower_state"])),
+            u"radiant_tower_status": towerStatus(True, bin(game[u"scoreboard"][u"radiant"][u"tower_state"]))}
+    return dictionary
